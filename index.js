8d3fe361a9cfeff6bcdd3b0b6504747bbd8f4582
Lucas Ferreria <ferreira@live.com>
2017-08-04 12:31:24 -0300

    conexão com heroku

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 "use strict";
 
+var express = require('express');
+var app     = express();
+
+app.listen(3000);
+
+app.set('port', (process.env.PORT || 5000));
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+
 //limpa a sting, itera as palavras e verifica se uma delas é "contato"
 
 function checkContato(string) {
